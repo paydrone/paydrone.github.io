cf83959bdb949c6b46fe9ef2431fabfa21f0e66d
Pedro <paydrone@gmail.com>
2020-07-16 16:35:22 -0400

    Update all HTML files to use templates

--- js/home.js
+++ js/home.js
@@ -1,3 +1,20 @@
+$(function(){$('#navi').load('naviTemplate.html');})
+
+var filename = location.pathname.split('/').slice(-1)[0]
+filename = filename.substring(0, filename.indexOf('.'))
+var quantity = parseInt($('#gridItems').attr('quantity'));
+console.log(quantity);
+
+for (i=1; i<quantity; i++) {
+    $('#gridItems').append(
+        `<div class='grid-item'>
+          <a href='images/`+filename+`_fullsize/`+i+`.jpg' data-fancybox='`+filename+`'>
+            <img src='images/`+filename+`/`+i+`.jpg' />
+          </a>
+        </div>`
+    )
+}
+
 // init Masonry
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
